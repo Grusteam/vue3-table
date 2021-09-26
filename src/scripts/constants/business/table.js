578576d12233777087fb5af83f7724d1d0e96571
Serge <sergeuniquite@gmail.com>
2021-09-26 13:26:14 +0300

    #business add requirements

--- src/scripts/constants/business/table.js
+++ src/scripts/constants/business/table.js
@@ -1,8 +1,19 @@
+const userModelFields = [
+  { id: 'id' },
+  { id: 'fullName' },
+  { id: 'birthDateHumanized' },
+];
+
 const tableFields = [
+  { id: 'fullName', name: 'Full Name' },
+  { id: 'birthDateHumanized', name: 'Birth Date' },
+];
+
+const searchFields = [
   { id: 'fullName', name: 'Full Name' },
   { id: 'birthDateHumanized', name: 'Birth Date' },
 ];
 
 const pageSize = 10;
 
-export { tableFields, pageSize };
+export { userModelFields, tableFields, searchFields, pageSize };
